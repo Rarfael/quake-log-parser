81a9d76679f7aa4b04af7a7f7fa28995955dcbfb
Rafael Godoy <contact@rafaelgodoy.com>
2024-08-20 19:41:02 -0300

    Readed log file

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
-=
+	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-=
+	file, err := os.Open("logs/qgames.log")
+	if err != nil {
+		log.Fatalf("Failed to open file: %s", err)
+	}
+	defer file.Close()
+	fmt.Println("File opened successfully")
 }
