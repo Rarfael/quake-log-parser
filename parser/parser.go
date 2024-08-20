7741cfecc7d15b8da45ea95a39969cd95669d797
Rafael Godoy <contact@rafaelgodoy.com>
2024-08-20 19:58:27 -0300

    Create Player struct

--- parser/parser.go
+++ parser/parser.go
@@ -2,7 +2,11 @@ package parser
 
 type Game struct {
 	TotalKills   int
-	Players      map[string]bool
-	Kills        map[string]int
+	Players      map[string]*Player
 	KillsByMeans map[string]int
+}
+
+type Player struct {
+	Name  string
+	Kills int
 }
